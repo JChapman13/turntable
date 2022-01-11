8842fb69d013deb68f887df3b1418bf6d618806c
JChapman13 <77743972+JChapman13@users.noreply.github.com>
2022-01-11 09:28:53 -0500

    Adding HomePage functionality

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ChannelJoinPage from "./ChannelJoinPage";
 import ChannelCreatePage from "./ChannelCreatePage";
 import Channel from "./Channel";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Routes,
@@ -14,12 +15,33 @@ export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Channel
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Channel
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
   render() {
     return (
       <>
         <Router>
           <Routes>
-            <Route path="/" element={<p>This is the homepage</p>} />
+            <Route path="/" element={this.renderHomePage()} />
             <Route path="/join" element={<ChannelJoinPage />} />
             <Route path="/create" element={<ChannelCreatePage />} />
             <Route path="/channel/:channelCode" element={<Channel />} />
